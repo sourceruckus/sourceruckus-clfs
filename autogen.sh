b659c688672aa678db68dbacdbd69ffa6e222638
Michael D Labriola <michael.d.labriola@gmail.com>
2014-09-06 15:23:24 -0400

    autotools: huge cleanup
    - Added --with-ruckus-dev and --with-ruckus-path.  This will allow us to
      store our ruckus src tree on a different disk than our target
      installation disk.
    
    - Removed half-baked --enable-install-* flags.  I'm going to create an
      external set of scripts to do all that stuff.  There's no longer any
      concept of an "install type".  The driver behind this is: boot off of
      LiveCD, clone wrapper script, execute it.  The script can then set up the
      HDDs appropriately and clone the whole ruckus tree into them AFTERWARDS.
      Otherwise, we have to be able to clone the whole ruckus tree on the
      LiveCD, which may not work (i.e., not enough drive space?).  Plus there
      as just way too much half-baked stuff in here... need to concentrate on
      just getting something building.
    
    - Fixed handling of precious variables.  They need to be set conditionally
      so the user can actually use them.
    
    - Now using xz -0 for compression of baby-bootstrap-stage1.tar

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-autoreconf -v --install --symlink || exit 1
+# FIXME: probably shouldn't symlink... once we boot into the stage2
+#        environment, the symlinks will be broken...
+autoreconf -v --install || exit 1
 
 if test -z "$NOCONFIGURE"; then
     ./configure "$@"
