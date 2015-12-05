76b41b9e27746010d239332ad53813155eeb7c73
Michael D Labriola <michael.d.labriola@gmail.com>
2015-12-04 20:36:30 -0500

    doom

--- utils/bootstrap-stage2/foo.c
+++ utils/bootstrap-stage2/foo.c
@@ -1,10 +1,10 @@
 /* multilib hello world
  *
- * gcc $BUILD64 -o foo64 foo.c
- * gcc $BUILD32 -o foo32 foo.c
+ * gcc $BUILD64 -D__BUILD=64 -o foo64 foo.c
+ * gcc $BUILD32 -D__BUILD=32 -o foo32 foo.c
  */
 #include <stdio.h>
 int main(int argc, char **argv) {
-	printf("hello world\n");
+	printf("hello world (%dbit)\n", __BUILD);
 	return 0;
 }
